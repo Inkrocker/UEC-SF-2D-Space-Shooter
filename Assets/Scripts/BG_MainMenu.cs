2016ec6996e6930bd653e74da53ca6f2ee522c5e
Bryan Internoscia <78615556+Inkrocker@users.noreply.github.com>
2021-02-24 03:30:23 -0500

    Updated MainMenu Animations Timing & Post Process FX

---
+++ Assets/Scripts/BG_MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private float _bgSpeed;
+
+    [SerializeField]
+    private GameObject _backgroundPrefab;
+
+    [SerializeField]
+    private GameObject _backgroundPrefab2;
+
+    private void Update()
+    {
+        BackgroundPosition();
+    }
+
+    void BackgroundPosition()
+    {
+        transform.Translate(_bgSpeed * Vector3.left * Time.deltaTime);
+
+        if(transform.localPosition.x < -1024f)
+        {
+            transform.localPosition = new Vector3(1024f, 0.0f, 0.0f);
+        }
+    }
+}
